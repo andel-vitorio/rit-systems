fe439c6e95f4614882b922cf19104026233652b6
Andevaldo Vitório <andevaldo.vitorio@icomp.ufam.edu.br>
2023-10-26 18:59:30 -0400

    fix: remove fab and topbar component from home screen

--- app/src/main/java/com/example/ritsystems/HomeFragment.java
+++ app/src/main/java/com/example/ritsystems/HomeFragment.java
@@ -1,6 +1,33 @@
 package com.example.ritsystems;
 
-import android.app.Activity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class HomeFragment extends Activity {
+import androidx.fragment.app.Fragment;
+
+import com.example.ritsystems.databinding.HomeFragmentBinding;
+
+public class HomeFragment extends Fragment {
+    private HomeFragmentBinding binding;
+
+    @Override
+    public View onCreateView(
+        LayoutInflater inflater, ViewGroup container,
+        Bundle savedInstanceState
+    ) {
+        binding = HomeFragmentBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
 }
